934b2db6a2905368728be8d426c6e1b71b7687fb
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-07-22 16:37:07 -0700

    Added hidden menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [hidden, setHidden] = useState(false);
+
+  function handleClick() {
+    setHidden(!hidden);
+  }
   return (
     <div>
       <header>
-        <Link to="/">
-          <div className="logo-container">Travel.</div>
-        </Link>
-        <nav>
-          <Link to="/about">
-            <div className="nav-link">About</div>
+        <div className="header-wrapper">
+          <Link to="/">
+            <div className="logo-container">Travel.</div>
           </Link>
-          <Link to="/news">
-            <div className="nav-link">News</div>
-          </Link>
-          <Link to="/contact">
-            <div className="nav-link">Contact</div>
-          </Link>
-          <div className="hamburger">
-            <div className="line"></div>
-            <div className="line half"></div>
-            <div className="line"></div>
+          <nav>
+            <Link to="/about">
+              <div className="nav-link">About</div>
+            </Link>
+            <Link to="/news">
+              <div className="nav-link">News</div>
+            </Link>
+            <Link to="/contact">
+              <div className="nav-link">Contact</div>
+            </Link>
+            <div className="hamburger" onClick={handleClick}>
+              <div className="line"></div>
+              <div className="line half"></div>
+              <div className="line"></div>
+            </div>
+          </nav>
+        </div>
+        <div
+          className="hidden-menu"
+          style={{
+            width: hidden ? "50%" : "0",
+            opacity: hidden ? 1 : 0,
+            pointerEvents: hidden ? "all" : "none",
+          }}
+        >
+          <div className="close-hidden-menu" onClick={handleClick}>
+            ✕
           </div>
-        </nav>
+          <div className="hidden-menu-text-container">
+            <Link to="/about">
+              <div className="nav-link" onClick={handleClick}>
+                About
+              </div>
+            </Link>
+            <Link to="/news">
+              <div className="nav-link" onClick={handleClick}>
+                News
+              </div>
+            </Link>
+            <Link to="/contact">
+              <div className="nav-link" onClick={handleClick}>
+                Contact
+              </div>
+            </Link>
+          </div>
+        </div>
       </header>
     </div>
   );

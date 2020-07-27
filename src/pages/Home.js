1f399058bf728432cf5a273548c8c4cc1b3b0f35
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-07-27 01:05:10 -0700

    Added form required

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -271,28 +271,28 @@ export default function Home() {
       </section>
       <section className="contact-banner" id="contact">
         <div className="wrapper">
-          <form className="contact-container">
+          <form className="contact-container" onSubmit={handleSubmit}>
             <h2>Get In Touch With Us</h2>
             <p>We'll get back to you as soon as possible</p>
             <div className="input-container">
               <label htmlFor="what">Full Name</label>
-              <input type="text" placeholder="Your Name" />
+              <input type="text" placeholder="Your Name" required="true" />
             </div>
             <div className="input-container">
               <label htmlFor="what">Email</label>
-              <input type="text" placeholder="Your Email" />
+              <input type="email" placeholder="Your Email" required="true" />
             </div>
             <div className="input-container">
               <label htmlFor="what">Phone Number</label>
-              <input type="text" placeholder="Your Phone Number" />
+              <input type="tel" placeholder="Your Phone Number" required="true" />
             </div>
             <div className="input-container">
               <label htmlFor="what">Travel Interests</label>
-              <input type="text" placeholder="Your Travel Interests" />
+              <input type="text" placeholder="Your Travel Interests" required="true" />
             </div>
             <div className="input-container">
               <label htmlFor="what">Number Of Travelers</label>
-              <input type="text" placeholder="How People Will Be Traveling" />
+              <input type="text" placeholder="How People Will Be Traveling" required="true"/>
             </div>
             <button type="submit">Submit</button>
           </form>
@@ -300,4 +300,9 @@ export default function Home() {
       </section>
     </div>
   );
+}
+
+const handleSubmit = e => {
+  e.preventDefault();
+  alert('Your message has been sent');
 }

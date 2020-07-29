3dab759e617d8a32c84b55363c7f2b64e8b2c157
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-07-29 00:45:46 -0700

    Updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./About.css";
+import house from "../images/icons/house.png";
+import worldMap from "../images/icons/worldMap.png";
+import moneyBag from "../images/icons/moneyBag.png";
 import instagram from "../images/icons/instagram.png";
 import facebook from "../images/icons/facebook.png";
 import linkedin from "../images/icons/linkedin.png";
@@ -10,6 +13,38 @@ import portrait4 from "../images/portraits/portrait4.png";
 export default function About() {
   return (
     <div>
+      <section className="info-banner">
+        <div className="info-banner-text-container">
+          <h2>Why Choose Us</h2>
+          <p>A subheading</p>
+        </div>
+        <div className="info-banner-card-container">
+          <div className="info-banner-card">
+            <img src={house} alt="house" />
+            <h2>Handpicked Hotels</h2>
+            <p>
+              Every journey is different. Find a home that puts you in the
+              correct headspace
+            </p>
+          </div>
+          <div className="info-banner-card">
+            <img src={worldMap} alt="world map" />
+            <h2>World Class Service</h2>
+            <p>
+              Our expert employees will take away all the travel stress you may
+              experience.
+            </p>
+          </div>
+          <div className="info-banner-card">
+            <img src={moneyBag} alt="Money Bag" />
+            <h2>Best Price Gaurantee</h2>
+            <p>
+              Our partnerships will ensure you get the best prices for all your
+              purchases.
+            </p>
+          </div>
+        </div>
+      </section>
       <section className="info-banner">
         <div className="info-banner-text-container">
           <h2>Our Team</h2>
